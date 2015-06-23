6487705123d7f1e9d6c04dbdb6e9eb687138e506
otiai10 <otiai10@gmail.com>
2015-06-23 20:55:04 +0900

    AVPlayerViewControllerを使えるようにして、プロパティを追加

---
+++ iTunesMusicSearch/DetailViewController.swift
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.swift
+//  iTunesMusicSearch
+//
+//  Created by Hiromu Ochiai on 2015/06/23.
+//  Copyright (c) 2015年 Hiromu Ochiai. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+import AVKit
+
+class DetailViewController: AVPlayerViewController {
+   
+    var trackName: String!
+    var previewUrl: String?
+}
