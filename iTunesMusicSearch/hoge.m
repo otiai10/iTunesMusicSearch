b54635cb829425dcf99cf4201002eff7be6273d6
otiai10 <otiai10@gmail.com>
2015-06-23 19:58:07 +0900

    hogeというObjective-Cファイルをつくることによって、Bridge-Header.hをつくらせた

---
+++ iTunesMusicSearch/hoge.m
@@ -0,0 +1,9 @@
+//
+//  hoge.m
+//  iTunesMusicSearch
+//
+//  Created by Hiromu Ochiai on 2015/06/23.
+//  Copyright (c) 2015年 Hiromu Ochiai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
