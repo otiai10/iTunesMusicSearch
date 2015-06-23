b313fffe79ea69bc9f131a24358d3e4682af5ed5
otiai10 <otiai10@gmail.com>
2015-06-23 19:58:57 +0900

    hogeはBridge-Headerがほしかっただけなので抹殺する

--- iTunesMusicSearch/hoge.m
+++
@@ -1,9 +0,0 @@
-//
-//  hoge.m
-//  iTunesMusicSearch
-//
-//  Created by Hiromu Ochiai on 2015/06/23.
-//  Copyright (c) 2015年 Hiromu Ochiai. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
